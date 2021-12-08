4b9a1f9f746d0c9fe09c1c1d05aff9799b231977
alixe <alcook090@gmail.com>
2021-12-08 17:39:54 +0000

    creating results page to link with API

--- website/views.py
+++ website/views.py
@@ -1,10 +1,16 @@
 from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
+videos = ["glxrwC9zsHY",
+          "oo9s2zg_nC4",
+          "-b2lNLq3EaA"]
+
 
 @views.route('/')
 def home():
     return render_template("home.html")
 
 
-
+@views.route('/results')
+def results():
+    return render_template("results.html", len=len(videos), videos=videos)
