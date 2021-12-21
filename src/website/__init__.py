42333b587bdc38c509b87b7340eaafd87a6dbbf7
Regina Osei-Bonsu <76152827+ReginaAbena19@users.noreply.github.com>
2021-12-21 19:09:29 +0000

    deleted some files, made database and login file and added necessary codes

--- src/website/__init__.py
+++ src/website/__init__.py
@@ -11,3 +11,4 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     return app
+
--- src/website/login/auth.py
+++ src/website/login/auth.py
@@ -2,27 +2,26 @@ from flask import Flask, render_template, redirect,url_for
 
 app = Flask (__name__, template_folder ='templates')
 
-
-@app.route('/')
+@app.route('/home')
 def index():
-    return render_template("base.html")
+    return render_template("home.html")
 
 @app.route('/login')
 def login():
     return render_template("login.html")
 
-@app.route('/home')
-def home():
-    return render_template("home.html")
-
 @app.route('/signup')
 def new_user():
-    return render_template("signup.html")
+    return render_template("sign_up.html")
 
 #if the login is successful
 @app.route('/profile')
 def user_profile():
     return render_template("profile.html")
+#e.g.
+@app.route('/login')
+def login():
+    return redirect(url_for('user_profile'))
 
 @app.route('/logout')
 def logout():
@@ -30,6 +29,3 @@ def logout():
 
 if __name__ =='__main__':
     app.run(debug=True)
-
-
-
