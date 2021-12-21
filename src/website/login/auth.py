237e5028cca8a86b572cb3f85426c77159e10590
Regina Osei-Bonsu <76152827+ReginaAbena19@users.noreply.github.com>
2021-12-21 20:11:52 +0000

    update

--- src/website/login/auth.py
+++ src/website/login/auth.py
@@ -1,31 +1,19 @@
-from flask import Flask, render_template, redirect,url_for
+from flask import Blueprint, render_template
 
-app = Flask (__name__, template_folder ='templates')
+auth = Blueprint('auth', __name__)
 
-@app.route('/home')
-def index():
-    return render_template("home.html")
-
-@app.route('/login')
+@auth.route('/login')
 def login():
     return render_template("login.html")
 
-@app.route('/signup')
-def new_user():
-    return render_template("sign_up.html")
-
-#if the login is successful
-@app.route('/profile')
-def user_profile():
-    return render_template("profile.html")
-#e.g.
-@app.route('/login')
-def login():
-    return redirect(url_for('user_profile'))
-
-@app.route('/logout')
+@auth.route('/logout')
 def logout():
     return render_template("logout.html")
 
-if __name__ =='__main__':
-    app.run(debug=True)
+@auth.route('/profile')
+def user_profile():
+    return render_template("profile.html")
+
+@auth.route('/sign-up')
+def sign_up():
+    return render_template("sign_up.html")
