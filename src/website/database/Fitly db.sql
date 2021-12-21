337370db444925ffdfe82a7351258c1f19c29cb0
Regina Osei-Bonsu <76152827+ReginaAbena19@users.noreply.github.com>
2021-12-21 20:39:57 +0000

    Update Fitly db.sql

--- src/website/database/Fitly db.sql
+++ src/website/database/Fitly db.sql
@@ -1,16 +1,14 @@
-CREATE DATABASE Fitly;
-
+Create Database Fitly
 use fitly;
-
-CREATE TABLE login_info(
-User_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-Username VARCHAR(100) NOT NULL,
-User_email VARCHAR(100) NOT NULL,
-User_password VARCHAR(100) NOT NULL
+CREATE TABLE login(
+id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(100) NOT NULL,
+email VARCHAR(100) NOT NULL,
+password VARCHAR(100) NOT NULL
  ) ENGINE = InnoDB AUTO_INCREMENT =2 DEFAULT CHARSET = utf8;
  
- INSERT INTO login_info
-(User_id, Username, User_email, User_password)
+ INSERT INTO login
+(id, firstname, email, password)
 VALUES
 (001, 'Alixe','Alixecook@gmail.com','Stawberry'),
 (002, 'Yasmine','Yasmineleghnider@gmail.com','Apple'),
@@ -19,12 +17,10 @@ VALUES
 (005, 'Raghad','Raghadzuraiki@gmail.com','Lemon');
 
 
-
 CREATE TABLE Youtube_results(
 Workout_name VARCHAR(100) NOT NULL,
 Workout_url VARCHAR(2048) NOT NULL
 );
-
 
 CREATE TABLE Exercise_db_results(
 Workout_name VARCHAR(100) NOT NULL,
@@ -32,7 +28,3 @@ Exercise_goal Varchar(100) NOT NULL,
 workout_demo_url VARCHAR(100) NOT NULL,
 Location Varchar(100) NOT NULL
 );
-
-
-
-
