6812a193431ed29ab061a1c1dbf7d5ac98b48651
Delia Neamtu-Cucu <90757890+DeliaNeamtu-Cucu@users.noreply.github.com>
2021-12-23 17:20:55 +0000

    updated to show random workout video

--- src/website/youtube/youtube_utils.py
+++ src/website/youtube/youtube_utils.py
@@ -43,14 +43,8 @@ class YoutubeWorkout:
         if self.number_of_videos == 3:
             for item in randomise_results:
                 url = ("https://www.youtube.com/watch?v=" + item["id"]["videoId"]),
-                urls.append(url)
-        else:
-            for item in randomise_results:
-                url = ("https://www.youtube.com/watch?v=" + item["id"]["videoId"])
-                urls.append(url)
+                urls.append(url)      
         self.url_to_db(urls)
-
-
 
     def get_workout_results_from_youtube(self, workout_type, number_of_videos):
         api_connection = self.connect_to_api()
@@ -65,6 +59,47 @@ class YoutubeWorkout:
         self.create_urls_list_in_db(randomised_results)
 
         return render_template('results.html', len=len(randomised_results), videos=randomised_results)
+    
+    def random_video_api(self, api_service_name, api_version, developer_key):
+        youtube = googleapiclient.discovery.build(
+            api_service_name,
+            api_version,
+            developerKey=developer_key
+        )
+        requests = youtube.search().list(
+            part="snippet",
+            maxResults=100,
+            q="workout",
+            order="date"
+        )
+        response_2 = requests.execute()
+        return response_2
+
+    def random_workout_result(self, response_2):
+        items = response_2["items"]
+        random_video = random.sample(items, 1)
+        return random_video
+
+    def get_random_workout(self, workout_type, number_of_videos):
+        api_connection = self.connect_to_api()
+
+        api_service_name = api_connection[0]
+        api_service_version = api_connection[1]
+        developer_key = api_connection[2]
+
+        random_api_result = self.random_video_api(api_service_name, api_service_version, developer_key)
+        random_workout = self.random_workout_result(random_api_result)
+
+        self.random_video_url_in_db(random_workout)
+
+        return render_template('results.html', len=len(random_workout), videos=random_workout)
+
+    def random_video_url_in_db(self, random_workout):
+        urls = []
+        for item in random_workout:
+            url = ("https://www.youtube.com/watch?v=" + item["id"]["videoId"])
+            urls.append(url)
+        self.url_to_db(urls)
 
     def url_to_db(self, urls):
         if request.method == 'POST':
