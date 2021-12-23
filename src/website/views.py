99df5ef8c96d5b8509f278ed3004cf4aceb94965
Yasmine Leghnider <yleghnider@gmail.com>
2021-12-23 10:48:41 +0000

    refactored written_workout.py to a new WrittenWorkout class

--- src/website/views.py
+++ src/website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, session
-from src.website.quiz.written_workout import get_written_workout
+from src.website.quiz.written_workout import WrittenWorkout
 from werkzeug.utils import redirect
 
 from src.website.youtube.youtube_utils import YoutubeWorkout
@@ -20,7 +20,8 @@ def results():
     workout_type = request.form['workout-location'] + "+" + request.form['muscle-group'] + "+" + "workout"
     workout_specified = request.form['submit-button']
     if workout_specified == "written":
-        return get_written_workout()
+        written_workout = WrittenWorkout()
+        return written_workout.get_written_workout()
     elif workout_specified == "personalised-youtube":
         youtube_workout = YoutubeWorkout(workout_type, 3)
         return youtube_workout.get_workout_results_from_youtube(workout_type, 3)
